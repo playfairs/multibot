07ca54987b6fdf4a6c08aa733b372a65ffbbc4b2
Playfair <74739795+playfairs@users.noreply.github.com>
2024-10-14 10:52:41 -0500

    Update embedresponse.py

--- cogs/embedresponse.py
+++ cogs/embedresponse.py
@@ -7,14 +7,14 @@ class EmbedResponse(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.content.lower() == "nuh uh" and not message.author.bot:
-            video_path = "C:\\Users\\fnafl\\Downloads\\heresy-main\\heresy-main\\nuhuh.mp4"
+        if message.content.lower() == "Keyword" and not message.author.bot:
+            video_path = "C:\\Users\\user\\path\\filename.mp4" # Replace with your actual file path, also make sure their \\ not \ for the bot to read them as a path
             await message.channel.send(file=discord.File(video_path))
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.content.lower() == "minion tampons" and not message.author.bot:
-            video_path = "C:\\Users\\fnafl\\Downloads\\heresy-main\\heresy-main\\miniontampons.mov"
+        if message.content.lower() == "keyword" and not message.author.bot:
+            video_path = "C:\\Users\\user\\path\\filename.mp4" # just do the same thing as previously mentioned
             await message.channel.send(file=discord.File(video_path))
 
 async def setup(bot):
